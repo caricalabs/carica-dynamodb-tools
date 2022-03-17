60c0a0103cc12066a12badaad719b6e83b6e900d
Shaw Terwilliger <sterwill@tinfig.com>
2022-03-17 07:43:10 -0400

    Add more item stats

--- carica_dynamodb_tools/stats.py
+++ carica_dynamodb_tools/stats.py
@@ -2,7 +2,7 @@ import csv
 import json
 import math
 import sys
-from typing import Tuple, Any, Iterator, Iterable
+from typing import Tuple, Iterator, Iterable
 
 import click
 
@@ -10,26 +10,33 @@ import carica_dynamodb_tools.version
 from carica_dynamodb_tools.item_size import item_size
 
 
-def attr_name(a: str) -> str:
-    return 'attr.' + a
-
-
-def attr_any_value(attr: dict) -> Any:
-    return next(iter(attr.values()))
-
-
 def generate_stats(attrs: Iterable[str]) -> Iterator[dict]:
     """Yield a dict containing stats for each item read from stdin."""
     for line in sys.stdin.readlines():
         item_stats = {}
         item = json.loads(line.strip())
-        if attrs:
-            item_stats.update({attr_name(a): attr_any_value(item[a]) for a in attrs})
 
+        # Add attributes specified on the command-line.  Prefix them with
+        # "attr." so they don't collide with other columns we add.
+        for a in attrs:
+            attr_val = item.get(a)
+            item_stats['attr.' + a] = str(next(iter(attr_val.values()))) if attr_val else ''
+
+        # Calculate the byte size of the item DynamoDB will charge us for, and
+        # a few derived statistics.
         size = item_size(item)
+        read_blocks_required = math.ceil(size / 4096)
+        read_bytes_required = read_blocks_required * 4096
+        write_blocks_required = math.ceil(size / 1024)
+        write_bytes_required = read_blocks_required * 1024
+
         item_stats['size'] = size
-        item_stats['read_units'] = math.ceil(size / 4096) / 2
-        item_stats['write_units'] = math.ceil(size / 1024)
+        item_stats['read_units'] = read_blocks_required / 2
+        item_stats['read_efficiency'] = size / read_bytes_required
+        item_stats['read_excess'] = read_bytes_required - size
+        item_stats['write_units'] = write_blocks_required
+        item_stats['write_efficiency'] = size / write_bytes_required
+        item_stats['write_excess'] = write_bytes_required - size
         yield item_stats
 
 
